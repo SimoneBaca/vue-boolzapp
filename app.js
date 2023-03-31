41c6c0ddc2851aefd9485f6c02dbf212ba4534d5
Simonebaca <simobaco99@gmail.com>
2023-03-31 10:58:41 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,2 @@
+/*Milestone 1 replica della grafica con la possibilità di avere messaggi scritti dall'utente (verdi) e dell'interlocutore(bianco )assegnando due classi css diverse.
+visualizzazione dinamica della lista contatti:tramite la direttiva v-for,visualizzare nome e immagine di ogni contatto.*/
